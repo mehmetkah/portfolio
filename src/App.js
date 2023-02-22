ff78c20f8756ecf77eb9a190d326aa847fcb52b7
“mehmetkah” <mehmetkahca@gmail.com>
2023-02-21 23:36:12 -0700

    deployingcommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Developing from "./components/Developing";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <main>
+        <Developing />
+      </main>
     </div>
   );
 }
